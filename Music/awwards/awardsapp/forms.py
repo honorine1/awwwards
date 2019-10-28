a4678e080ddc5190923182062662c16e8d923904
honorine1 <uwurukundoh@gmail.com>
2019-10-28 12:43:11 +0200

    forms.py is added

---
+++ Music/awwards/awardsapp/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Profile,Projects
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+class ProjectsForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        exclude = ['profile','user', 'created_date']
+        
+
